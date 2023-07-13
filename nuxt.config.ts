619aacf0494c09dc24c347b8869d3b6300ffe64a
AmmarM0hammed <108635464+AmmarM0hammed@users.noreply.github.com>
2023-07-13 16:18:05 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfig({
+  // https://github.com/nuxt-themes/docus
+  extends: '@nuxt-themes/docus',
+
+  css:['~/assets/css/main.css'],
+  modules: [
+    // https://github.com/nuxt-modules/plausible
+    '@nuxtjs/plausible',
+    // https://github.com/nuxt/devtools
+    // '@nuxt/devtools'
+  ]
+})
